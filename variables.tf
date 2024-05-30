ebff1102afedd19dc901a14f3bfdf28d18fd11d4
Darthicks <153494439+Darthicks@users.noreply.github.com>
2024-05-30 15:59:55 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,17 @@
-variable "blob_container_name" {
-  description = "The name of the blob container"
+variable "rg_name" {
+  description = "The name of the resource group"
 }
 
-variable "environment" {
-  description = "The environment name"
+variable "location" {
+  description = "The location for Azure resources"
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+}
+
+variable "blob_container_name" {
+  description = "The name of the blob container"
 }
 
 variable "storage_account_access_key" {
@@ -14,30 +22,22 @@ variable "storage_account_name" {
   description = "The name of the storage account"
 }
 
-variable "location" {
-  description = "The location for the resources"
+variable "environment" {
+  description = "The environment (e.g., dev, staging, prod)"
 }
 
 variable "loader_image" {
-  description = "The image for the loader container"
+  description = "The Docker image for the loader container"
 }
 
 variable "ui_image" {
-  description = "The image for the UI container"
+  description = "The Docker image for the UI container"
 }
 
 variable "maintenance_image" {
-  description = "The image for the maintenance container"
+  description = "The Docker image for the maintenance container"
 }
 
 variable "rest_image" {
-  description = "The image for the REST container"
-}
-
-variable "subnet_name" {
-  description = "The name of the subnet"
-}
-
-variable "rg_name" {
-  description = "The name of the resource group"
+  description = "The Docker image for the REST container"
 }
