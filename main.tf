c6de4c727778d10605ec3f20b5c02882599c5957
Darthicks <153494439+Darthicks@users.noreply.github.com>
2024-05-30 16:30:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "azurerm_storage_container" "blob_container" {
   container_access_type = "private"
 }
 
-rresource "azurerm_linux_function_app" "function_app" {
+resource "azurerm_linux_function_app" "function_app" {
   name                       = "myFunctionApp"
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
