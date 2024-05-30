3f6b84a0b1a6bcbf04ecfb197954372f0b1cca54
Darthicks <153494439+Darthicks@users.noreply.github.com>
2024-05-30 15:37:43 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,23 @@ variable "storage_account_access_key" {
 variable "storage_account_name" {
   description = "The name of the storage account"
 }
+
+variable "location" {
+  description = "The location for the resources"
+}
+
+variable "loader_image" {
+  description = "The image for the loader container"
+}
+
+variable "ui_image" {
+  description = "The image for the UI container"
+}
+
+variable "maintenance_image" {
+  description = "The image for the maintenance container"
+}
+
+variable "rest_image" {
+  description = "The image for the REST container"
+}
