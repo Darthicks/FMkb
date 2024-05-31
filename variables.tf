7b498e47e6ff7b138a8652a8c67d319801a5331f
Darthicks <153494439+Darthicks@users.noreply.github.com>
2024-05-31 13:46:14 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,43 +1,64 @@
-variable "rg_name" {
-  description = "The name of the resource group"
+variable "environment" {
+  description = "The environment for the resources (Dev/Acc/Prod)"
+  type        = string
 }
 
-variable "location" {
-  description = "The location for Azure resources"
+variable "location_code" {
+  description = "The location code (e.g., weeu)"
+  type        = string
+}
+
+variable "random_number" {
+  description = "A random number for uniqueness"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for VM"
+  type        = string
 }
 
 variable "subnet_name" {
   description = "The name of the subnet"
-}
-
-variable "blob_container_name" {
-  description = "The name of the blob container"
-}
-
-variable "storage_account_access_key" {
-  description = "The access key for the storage account"
+  type        = string
 }
 
 variable "storage_account_name" {
   description = "The name of the storage account"
+  type        = string
 }
 
-variable "environment" {
-  description = "The environment (e.g., dev, staging, prod)"
+variable "blob_container_name" {
+  description = "The name of the blob container"
+  type        = string
 }
 
 variable "loader_image" {
-  description = "The Docker image for the loader container"
+  description = "The image for the loader container"
+  type        = string
 }
 
 variable "ui_image" {
-  description = "The Docker image for the UI container"
+  description = "The image for the UI container"
+  type        = string
 }
 
 variable "maintenance_image" {
-  description = "The Docker image for the maintenance container"
+  description = "The image for the maintenance container"
+  type        = string
 }
 
 variable "rest_image" {
-  description = "The Docker image for the REST container"
+  description = "The image for the rest container"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure location for the resources"
+  type        = string
 }
