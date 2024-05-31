58f76d4c1c68795f574ef51803dd5ac62e7bb4bf
Darthicks <153494439+Darthicks@users.noreply.github.com>
2024-05-31 15:17:15 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,64 +1,68 @@
 variable "environment" {
-  description = "The environment for the resources (Dev/Acc/Prod)"
-  type        = string
-}
-
-variable "location_code" {
-  description = "The location code (e.g., weeu)"
-  type        = string
-}
-
-variable "random_number" {
-  description = "A random number for uniqueness"
-  type        = string
-}
-
-variable "admin_username" {
-  description = "Admin username for VM"
-  type        = string
-}
-
-variable "admin_password" {
-  description = "Admin password for VM"
-  type        = string
-}
-
-variable "subnet_name" {
-  description = "The name of the subnet"
-  type        = string
-}
-
-variable "storage_account_name" {
-  description = "The name of the storage account"
-  type        = string
-}
-
-variable "blob_container_name" {
-  description = "The name of the blob container"
-  type        = string
-}
-
-variable "loader_image" {
-  description = "The image for the loader container"
-  type        = string
-}
-
-variable "ui_image" {
-  description = "The image for the UI container"
-  type        = string
-}
-
-variable "maintenance_image" {
-  description = "The image for the maintenance container"
-  type        = string
-}
-
-variable "rest_image" {
-  description = "The image for the rest container"
+  description = "Environment (Dev/Acc/Prod)"
   type        = string
 }
 
 variable "location" {
-  description = "The Azure location for the resources"
+  description = "Azure location"
   type        = string
+}
+
+variable "random_number" {
+  description = "Random number for resource naming"
+  type        = string
+}
+
+variable "rg_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = ""
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+  default     = ""
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+  default     = ""
+}
+
+variable "blob_container_name" {
+  description = "Name of the blob container"
+  type        = string
+  default     = ""
+}
+
+variable "storage_account_access_key" {
+  description = "Access key for the storage account"
+  type        = string
+  default     = ""
+}
+
+variable "loader_image" {
+  description = "Docker image for loader container"
+  type        = string
+  default     = ""
+}
+
+variable "ui_image" {
+  description = "Docker image for UI container"
+  type        = string
+  default     = ""
+}
+
+variable "maintenance_image" {
+  description = "Docker image for maintenance container"
+  type        = string
+  default     = ""
+}
+
+variable "rest_image" {
+  description = "Docker image for rest container"
+  type        = string
+  default     = ""
 }
