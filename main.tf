0c525418e57a2dae451170da8fa1513a0d2d7449
Darthicks <153494439+Darthicks@users.noreply.github.com>
2024-06-03 10:18:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 provider "azurerm" {
   features {}
-
   skip_provider_registration = true
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source ="hashicorp/azurerm"
+      version ="=2.81.0"
+    }
+  }
 }
 
 # Random number resource
