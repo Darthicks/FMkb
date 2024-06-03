3079c7dc2083ef6590563f1475b8caa309c7eba5
Darthicks <153494439+Darthicks@users.noreply.github.com>
2024-06-03 13:56:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,6 +38,13 @@ resource "azurerm_virtual_network" "vnet" {
     address_prefix = "10.0.1.0/24"
   }
 }
+# Subnet
+resource "azurerm_subnet" "subnet" {
+ name                 = lower(format("fmkb_subnet_%s_%s_%s", var.environment, random_string.random.result, var.location))
+ resource_group_name  = azurerm_resource_group.rg.name
+ virtual_network_name = azurerm_virtual_network.vnet.name
+ address_prefixes     = ["10.0.1.0/24"]
+}
 
 # Network Interface
 resource "azurerm_network_interface" "vm_nic" {
